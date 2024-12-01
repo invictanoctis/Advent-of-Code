19fd2790dc5d73f0a46c9e5c8f233ee09596d5ed
Maximilian Menne <menne.max05@gmail.com>
2024-12-01 13:09:49 +0100

    removed goofy comments

--- day1/main1.py
+++ day1/main1.py
@@ -2,28 +2,27 @@ leftlist = []
 rightlist = []
 
 with open('input.txt', 'r') as file:
-    lines = file.readlines() # speichert jede Zeile als object in einer liste
+    lines = file.readlines()
 
 def assignlist():
     for numbers in lines:
-        numbers_seperated = str(numbers).split("   ") # macht eine Liste aus jeder Zeile, Trennung an 3 leerzeichen
+        numbers_seperated = str(numbers).split("   ")
         leftlist.append(numbers_seperated[0])
         rightlist.append(numbers_seperated[1])
 
-def sortlist(list): # bisschen unnoetig aber who cares
+def sortlist(list):
     list = list.sort()
 
 def difference():
     max_dif = 0
     for index in range(len(leftlist)):
         dif = int(leftlist[index]) - int(rightlist[index])
-        max_dif += abs(dif) # macht positiv und fuegt zur max dif hinzu
+        max_dif += abs(dif)
     return max_dif
+
 
 if __name__ == '__main__':
     assignlist()
     sortlist(leftlist)
     sortlist(rightlist)
     print(difference())
-
-# pythonic ahhhh loesung lol
